d908064d4bc7d04024c8783425ca14d04995afd4
Apon <temptemporary362@gmail.com>
2025-04-19 18:22:38 +0600

    Add files via upload

---
+++ Testing/App.jsx
@@ -0,0 +1,14 @@
+import React from 'https://esm.sh/react@19';
+import ReactDOM from 'https://esm.sh/react-dom@19/client';
+import Header from "Header.jsx";
+const App = () {
+    return (
+    <>
+        <h2>The heading, this is from App.jsx</h2>
+        <p>And a paragraph from App too</p>
+        <Header />
+    </>
+    );
+};
+
+export default App;
---
+++ Testing/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'https://esm.sh/react@19';
+import ReactDOM from 'https://esm.sh/react-dom@19/client';
+
+const Header = () {
+    return (
+    <>
+        <h2>The heading, this is from Header.jsx</h2>
+        <p>And a paragraph from Header too</p>
+    </>
+    );
+};
+
+export default Header;
