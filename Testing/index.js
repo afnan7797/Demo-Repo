f0e8895938ff5add2627f0deccf273b2f0b74978
Apon <temptemporary362@gmail.com>
2025-04-19 18:28:25 +0600

    Update index.js

--- Testing/index.js
+++ Testing/index.js
@@ -1,5 +1,5 @@
 import React from 'https://esm.sh/react@19';
 import ReactDOM from 'https://esm.sh/react-dom@19/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-import App from "App.jsx";
+import App from "./App.jsx";
 root.render(<App />);
